91a1445a34007088589f5d955bddf9237087625f
Uzbagoitel777 <uzb777b@gmail.com>
2024-10-15 21:07:26 +0500

    Separated a few functions into a js file for reuse in other scripts

---
+++ public/shared.js
@@ -0,0 +1,66 @@
+export async function fetchOfferDetails(id) {
+    try {
+        const response = await fetch(`/api/offers/${id}`);
+        const data = await response.json();
+        if (data.success) {
+            renderOfferDetails(data.offer);
+        } else {
+            console.error('Failed to fetch offer details:', data.error);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+export function renderOfferDetails(offer) {
+    const detailsContainer = document.getElementById('offer-details-container');
+    
+    const elements = [];
+    
+    const titleElement = document.getElementById('offer-title');
+    titleElement.textContent = offer.title;
+    elements.push(titleElement);
+    
+    const briefElement = document.createElement('p');
+    briefElement.textContent = offer.brief;
+    briefElement.style.color = '#6c757d';
+    elements.push(briefElement);
+
+    const imageElement = document.createElement('img');
+    imageElement.src = 'placeholder.png';
+    imageElement.alt = 'Offer image';
+    elements.push(imageElement);
+
+    const salaryElement = document.createElement('p');
+    if (offer.salary_upper <= 0 || (!offer.salary_upper && !offer.salary_lower)) {
+        salaryElement.textContent = 'Unpaid internship';
+        salaryElement.style.color = '#6c757d';
+    } else if (offer.salary_lower && offer.salary_upper) {
+        salaryElement.textContent = `Salary: ${offer.salary_lower} - ${offer.salary_upper} RUB`;
+    } else if (offer.salary_lower) {
+        salaryElement.textContent = `Salary: more than ${offer.salary_lower} RUB`;
+    } else if (offer.salary_upper) {
+        salaryElement.textContent = `Salary: up to ${offer.salary_upper} RUB`;
+    }
+    salaryElement.className = 'offer-salary';
+    elements.push(salaryElement);
+
+    const textElement = document.createElement('p');
+    textElement.textContent = offer.desc;
+    elements.push(textElement);
+
+    if(document.URL === 'http://localhost:3000/'){
+        const applyButton = document.createElement('button');
+        applyButton.textContent = 'Send application';
+        applyButton.className = 'btn btn-primary';
+        applyButton.addEventListener('click', () => {
+            window.location.href = `/submit_application.html?offerId=${offer.ID}`;
+        });    
+        elements.push(applyButton);
+    }
+
+    detailsContainer.innerHTML = '';
+    elements.forEach(element => detailsContainer.appendChild(element));
+
+    detailsContainer.classList.remove('d-none');
+}
